0406ebfea0a1476fc5783f913b4806b6fc26c6e5
OptionSelect <woofenwauzer@gmail.com>
2017-10-02 19:01:15 -0400

    getting db errors

--- HealthTracker/Startup.cs
+++ HealthTracker/Startup.cs
@@ -25,7 +25,7 @@ namespace HealthTracker
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddDbContext<FormDataContext>(opt => opt.UseInMemoryDatabase("FormDataList"));
+            services.AddDbContext<FormDataContext>();
             services.AddMvc();
         }
 
